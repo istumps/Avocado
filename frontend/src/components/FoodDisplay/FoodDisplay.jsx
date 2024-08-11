4398c0859c6900122fd7af014b56cf7ab3014355
istumps <istumps@csu.fullerton.edu>
2024-08-10 22:04:15 -0700

    added loading state

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,15 +1,42 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
+import Spinner from '../Spinner/Spinner'
+
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const FoodDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)
+    const [loading, setLoading] = useState(true);
+    
+    if (!food_list || food_list.length === 0) {
+      toast.info("Please wait up to 50 seconds to receive the updated menu due to free hosting delays.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+  
+      return (
+        <div className="food-display centered" id="food-display">
+          <ToastContainer />
+          <div className="spinner"></div> 
+        </div>
+      );
+    }
 
   return (
     <div className='food-display' id="food-display">
         <h2>Top dishes near you</h2>
+        <ToastContainer />
         <div className="food-display-list">
             {food_list.map((item, index) => {
               if(category==="All" || category === item.category){
---
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+// Spinner.js
+import React from 'react';
+import './Spinner.css'; // Add spinner styles here
+
+const Spinner = () => {
+  return <div className="spinner"></div>;
+};
+
+export default Spinner;
